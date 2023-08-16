df69d1451d651167ddf90ccc64d379532d9b7c7c
Evgeny <evgenybashura@gmail.com>
2023-08-16 18:53:53 +0300

    django_likes_and_comments

---
+++ src/manager/management/commands/add_data_to_db.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+from manager.models import Book
+
+
+# from polls.models import Question as Poll
+
+
+class Command(BaseCommand):
+    help = "add simple data to database"
+
+    def handle(self, *args, **options):
+        Book.objects.all().delete()
+        # User.objects.create(username='Max', password='useruser')
+        # user1 = User(username='andru', password='useruser')
+        # user2 = User(username='udzhin', password='useruser')
+        # user3 = User(username='user123', password='useruser')
+        # User.objects.bulk_create([user1, user2, user3])
+        users = User.objects.all()
+        book1 = Book(title='book1', text='text for book 1')
+        book2 = Book(title='book2', text='text for book 2')
+        book3 = Book(title='book3', text='text for book 3')
+        Book.objects.bulk_create([book1, book2, book3])
+
+        for book in Book.objects.all():
+            book.authors.add(users[0], users[2])
+            book.save()
